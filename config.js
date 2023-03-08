428bc8244308f2d0fe337540390b9de532f6c030
1715123329a <52093982+1715123329a@users.noreply.github.com>
2023-03-08 15:05:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
 export const BASE_URL = 'http://192.168.1.101:2022/api/applet'
 
-export const MAP_KEY = 'da12f1714499c255c70a5a7c018a45b5'  // 高德key
+export const MAP_KEY = 'da12f1714499c255c70a5a7'  // 高德key
